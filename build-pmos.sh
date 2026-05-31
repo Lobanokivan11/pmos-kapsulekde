1315c2d351d3a29419b79ebc8c3cfca0cda746be
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-05-31 15:38:26 +0300

    Update build-pmos.sh

--- build-pmos.sh
+++ build-pmos.sh
@@ -22,6 +22,7 @@ cp "$SCRIPT_DIR/setup-rootfs.sh" "$ROOTFS_DIR/"
 cp /etc/resolv.conf "$ROOTFS_DIR/etc/resolv.conf"
 chroot "$ROOTFS_DIR" /bin/sh /setup-rootfs.sh
 rm -f "$ROOTFS_DIR/setup-rootfs.sh" "$ROOTFS_DIR/etc/resolv.conf"
+mkdir -p "$SCRIPT_DIR/pmos-cache"
 mkosi --directory="$SCRIPT_DIR" --image="$IMAGE_NAME" build
 
 
