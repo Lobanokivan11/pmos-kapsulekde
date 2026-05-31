a69963883a3435a51661c38eba9a0e6ead6da5fc
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-05-31 14:36:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-export ROOTFS_DIR="out/pmos-rootfs"
-cp setup-rootfs.sh "$ROOTFS_DIR/"
-sudo chroot "$ROOTFS_DIR" /bin/sh /setup-rootfs.sh
-sudo rm "$ROOTFS_DIR/setup-rootfs.sh"
+ROOTFS_DIR="$(pwd)/out/pmos-rootfs"
+mkdir -p "$ROOTFS_DIR"
+wget https://dl-cdn.alpinelinux.org/alpine/edge/main/x86_64/apk-tools-static-3.0.6-r0.apk
+tar -xzf apk-tools-static-3.0.6-r0.apksbin/apk.static
+rm apk-tools-static-3.0.6-r0.apk
+sudo ./sbin/apk.static --repository https://dl-cdn.alpinelinux.org/alpine/edge/testing --repository https://dl-cdn.alpinelinux.org/alpine/edge/community --repository https://dl-cdn.alpinelinux.org/alpine/edge/main --update-cache --allow-untrusted --root "$ROOTFS_DIR" --initdb add alpine-base
+rm -rf sbin
