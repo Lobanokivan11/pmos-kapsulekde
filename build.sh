83870fa9213a4f6349ae31fe30003f59b85d7582
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-05-31 14:32:08 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-ROOTFS_DIR="out/pmos-rootfs"
+export ROOTFS_DIR="out/pmos-rootfs"
 cp setup-rootfs.sh "$ROOTFS_DIR/"
 sudo chroot "$ROOTFS_DIR" /bin/sh /setup-rootfs.sh
 sudo rm "$ROOTFS_DIR/setup-rootfs.sh"
